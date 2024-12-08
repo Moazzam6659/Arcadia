6420c16ab5f22eea6e4d541f0436bfc39a09e30f
Moazzam <moazzam.ali6659@gmail.com>
2024-12-08 14:37:19 +0530

    Update contact section padding on mobile device

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import ImageClipBox from './ImageClipBox'
 
 const Contact = () => {
   return (
-    <div id='contact' className='my-20 min-h-96 w-screen px-10'>
+    <div id='contact' className='my-20 min-h-96 w-screen px-5 md:px-10'>
       <div className='relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden'>
         <div className='absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96'>
           <ImageClipBox src='/img/contact-1.webp' alt='contact-1' clipClass='contact-clip-path-1' />
